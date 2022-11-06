2052736db355b0cb731628e49fa6b7bca212d36f
Kyrylo <112381397+KyryloStroiev@users.noreply.github.com>
2022-11-06 15:33:18 +0200

    Update BuyerAdd.cs

--- HW13/BuyerAdd.cs
+++ HW13/BuyerAdd.cs
@@ -20,23 +20,23 @@ public class BuyerAdd : IBuyer
         LastName = lastName;
         PhoneNumber = phoneNumber;
     }
-    //public void AddBuyer(string path)
-    //{
-    //    var buyeradd = new BuyerAdd(IdBuyer, FirsName, LastName, PhoneNumber);
-    //    var AddFile = @"C:\Users\User\Desktop\HM\HW13\HW13\Product.txt";
-    //    Console.WriteLine("Ведiть данi покупця:");
-    //    Console.WriteLine("Код покупця, ім'я, прізвище, номер телефона");
-    //    string idbuyer = Console.ReadLine();
-    //    IdBuyer = Convert.ToInt32(idbuyer);
-    //    FirsName = Console.ReadLine();
-    //    LastName = Console.ReadLine();
-    //    PhoneNumber = Console.ReadLine();
+  public void AddBuyer(string path)
+    {
+        var buyeradd = new BuyerAdd(IdBuyer, FirsName, LastName, PhoneNumber);
+        var AddFile = @"C:\Users\User\Desktop\HM\HW13\HW13\Product.txt";
+        Console.WriteLine("Ведiть данi покупця:");
+        Console.WriteLine("Код покупця, ім'я, прізвище, номер телефона");
+        string idbuyer = Console.ReadLine();
+        IdBuyer = Convert.ToInt32(idbuyer);
+        FirsName = Console.ReadLine();
+        LastName = Console.ReadLine();
+        PhoneNumber = Console.ReadLine();
 
-    //    File.AppendAllLines(path, new[] { $"{IdBuyer}, {FirsName}, {LastName}, {PhoneNumber}" });
-       
-    //}
-    //public override string ToString()
-    //    => $"{IdBuyer}, {FirsName}, {LastName}, {PhoneNumber}";
+        File.AppendAllLines(path, new[] { $"{IdBuyer}, {FirsName}, {LastName}, {PhoneNumber}" });
+
+    }
+    public override string ToString()
+        => $"{IdBuyer}, {FirsName}, {LastName}, {PhoneNumber}";
 
 
 
